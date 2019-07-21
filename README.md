acdf6154656424d693997294ec5fa15077eddc94
Faris Han <farishanugraha@gmail.com>
2019-07-21 23:49:10 +0700

    minor edit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 This is a starter template for Kongregate game without canvas.
+
+Features:
+- Area/screen navigation
+
+To-do List:
+- Save/Load
+- Import/Export
--- main.js
+++ main.js
@@ -5,11 +5,21 @@ document.getElementById('mainMenu').style.display = 'block';
 
 var player = {
 	name: "Default"
-}
+};
+
+var game = {
+	player: player
+};
 
 window.onload = (() => {
 	setButtonListener();
 })();
+
+function save(){
+	console.log(game)
+
+	// TODO: convert game object into base64 string
+};
 
 function setButtonListener(){
 	var buttons = document.querySelectorAll('button');
@@ -20,16 +30,16 @@ function setButtonListener(){
 				hideAreas();
 				document.getElementById(e.target.dataset.target).style.display = 'block';
 			}
-		})
+		});
 	}
-}
+};
 
 function hideAreas(){
 	var areas = document.querySelectorAll('.area');
 	for (var i = 0; i < areas.length; i++) {
 		areas[i].style.display = 'none';
 	}
-}
+};
 
 function ID() {
 	return '_' + Math.random().toString(36).substr(2, 9);
