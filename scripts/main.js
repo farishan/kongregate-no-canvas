5e855810829cc10efbd2454384470edadacec5fe
farishan <farishanugraha@gmail.com>
2019-12-02 21:39:45 +0700

    Add setting notification

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ const gameTitle = 'Game Title';
 document.title = gameTitle;
 document.getElementById('gameTitle').innerHTML = gameTitle;
 document.getElementById('mainMenu').style.display = 'block';
+const settingMessage = document.getElementById('settingMessage');
 
 var screens = document.querySelectorAll('.screen');
 
@@ -17,15 +18,6 @@ window.onload = (() => {
 	setScreenNavigators();
 })();
 
-function save(){
-	localStorage.setItem(gameTitle.split(' ').join('') + '_data', JSON.stringify(game));
-};
-
-function load(){
-	let data = JSON.parse(localStorage.getItem(gameTitle.split(' ').join('') + '_data'));
-	console.log(data);
-};
-
 function setScreenNavigators(){
 	var buttons = document.querySelectorAll('button');
 	for (var i = 0; i < buttons.length; i++) {
@@ -39,17 +31,34 @@ function setScreenNavigators(){
 	}
 };
 
+function save(){
+	localStorage.setItem(gameTitle.split(' ').join('') + '_data', JSON.stringify(game));
+
+	pushSettingNotification('saved');
+};
+
+function load(){
+	let data = JSON.parse(localStorage.getItem(gameTitle.split(' ').join('') + '_data'));
+
+	pushSettingNotification('loaded');
+};
+
 function exportData(obj){
 	document.getElementById('b64').value = btoa(JSON.stringify(game));
-	
+
 	// return btoa(JSON.stringify(obj));
+
+	pushSettingNotification('exported');
 };
 
 function importData(b64){
 	let data = atob(document.getElementById('b64').value);
 	game = JSON.parse(data);
+	console.log(game)
 
 	// return JSON.parse(atob(b64));
+
+	pushSettingNotification('imported');
 };
 
 function hideElements(elements){
@@ -57,6 +66,13 @@ function hideElements(elements){
 		elements[i].style.display = 'none';
 	}
 };
+
+function pushSettingNotification(message){
+	settingMessage.innerHTML = message;
+	setTimeout(function(){
+		settingMessage.innerHTML = '';
+	}, 1000);
+}
 
 function ID() {
 	return '_' + Math.random().toString(36).substr(2, 9);
